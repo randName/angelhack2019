574afbb4d3608c848da2ef0a03bf4ccfeb056851
Shun Git <kwokshungit@gmail.com>
2019-07-06 21:42:10 +0800

    pull out ItemCard

---
+++ src/components/ItemCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <v-card-title>
+      <div class="headline">{{ item.title }}</div>
+    </v-card-title>
+    <v-img v-if="item.image" :src="item.image" contain height="200px"></v-img>
+    <v-card-text>
+      {{ item.content }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  computed: {
+    item () {
+      return JSON.parse(this.value.content)
+    }
+  },
+}
+</script>
--- src/views/Items.vue
+++ src/views/Items.vue
@@ -3,14 +3,22 @@
     <v-layout row>
       <v-flex xs12>
         <v-card>
-          <amplify-connect :mutation="createItem" @done="content = null">
+          <amplify-connect :mutation="createItem" @done="done">
             <template #default="{ loading, mutate, errors }">
               <v-card-title>New Item</v-card-title>
               <v-card-text>
+                <v-text-field
+                  v-model="title"
+                  label="Title"
+                  :rules="[filled]"
+                ></v-text-field>
+                <v-text-field
+                  v-model="image"
+                  label="Image"
+                ></v-text-field>
                 <v-textarea
                   v-model="content"
                   label="Content"
-                  :rules="[filled]"
                 ></v-textarea>
               </v-card-text>
               <v-card-actions>
@@ -24,19 +32,11 @@
         </v-card>
       </v-flex>
     </v-layout>
-    <amplify-connect
-      :query="listItems"
-      :subscription="itemSubscription"
-      :onSubscriptionMsg="onCreateItem"
-    >
+    <amplify-connect :query="listItems" :subscription="itemSub" :onSubscriptionMsg="created">
       <template #default="{ loading, data: { listItems: items }, errors }">
         <v-layout v-if="items" row wrap>
           <v-flex v-for="item in items.items" :key="item.id" xs12 sm6>
-            <v-card>
-              <v-card-text>
-                {{ item.content }}
-              </v-card-text>
-            </v-card>
+            <ItemCard :value="item"></ItemCard>
           </v-flex>
         </v-layout>
       </template>
@@ -49,25 +49,45 @@ import { listItems } from '@/graphql/queries'
 import { createItem } from '@/graphql/mutations'
 import { onCreateItem } from '@/graphql/subscriptions'
 
+import ItemCard from '@/components/ItemCard'
+
 export default {
+  components: {
+    ItemCard,
+  },
   data: () => ({
     errors: [],
+    title: null,
+    image: null,
     content: null,
     filled: (v) => !!v || 'Cannot be blank'
   }),
   computed: {
+    item () {
+      const content = JSON.stringify({
+        title: this.title,
+        image: this.image,
+        content: this.content,
+      })
+      return { input: { content } }
+    },
     listItems () {
       return this.$Amplify.graphqlOperation(listItems)
     },
-    itemSubscription () {
+    itemSub () {
       return this.$Amplify.graphqlOperation(onCreateItem)
     },
     createItem () {
-      return this.$Amplify.graphqlOperation(createItem, { input: { content: this.content } })
+      return this.$Amplify.graphqlOperation(createItem, this.item)
     },
   },
   methods: {
-    onCreateItem ({ data }, { onCreateItem }) {
+    done () {
+      this.title = ''
+      this.image = null
+      this.content = null
+    },
+    created ({ data }, { onCreateItem }) {
       data.listItems.items.push(onCreateItem)
       return data
     },
